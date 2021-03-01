67070629597e0540b2b90a11025f5dc7aac9f866
“Alyssa <agoody44@gmail.com>
2021-03-01 18:47:40 -0500

    Adds search functionality

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
         .then(result => {
           setWeather(result);
           setQuery('');
-          console.log(result);
+          // console.log(result);
         });
     }
   }
@@ -50,15 +50,19 @@ function App() {
         /> 
 
       </div>
-      <div className="location-box">
-        <div className='location'>Boston, US</div>
-        <div className='date'>{dateDisplay(new Date())}</div>
+      {(typeof weather.main != 'undefined') ? (
+      <div>
+        <div className="location-box">
+          <div className='location'>{weather.name}, {weather.sys.country} </div>
+          <div className='date'>{dateDisplay(new Date())}</div>
+        </div>
+        <div className='weather-box'>
+          <div className='temp'>{Math.round(weather.main.temp)}°F</div>
+          <div className='weather'>{weather.weather[0].main}</div>
+          {/* <div className='icon'>{weather.weather[0].icon}</div> */}
+        </div>
       </div>
-      <div className='weather-box'>
-        <div className='temp'>Temperature = 50°F</div>
-        <div className='weather'>Sunny</div>
-
-      </div>
+      ) : ('')}
     </main>
     </div>
   );
