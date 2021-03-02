52b3767fe3ed16289b72af50f7ede54db9c45027
“Alyssa <agoody44@gmail.com>
2021-03-01 19:26:53 -0500

    Cleans up files

--- src/App.js
+++ src/App.js
@@ -1,75 +1,12 @@
-import React, {useState} from 'react';
-
 import './App.css';
-
-// API KEY AND URL
-const api ={
-  key: process.env.REACT_APP_API_KEY,
-  base: 'https://api.openweathermap.org/data/2.5/'
-}
-
+import WeatherCard from './components/WeatherCard'
 
 function App() {
-  const [query, setQuery] = useState('');
-  const [weather, setWeather] = useState({});
-
-  const search = event => {
-    if (event.key === 'Enter') {
-      fetch(`${api.base}weather?q=${query}&units=imperial&APPID=${api.key}`)
-        .then(res => res.json())
-        .then(result => {
-          setWeather(result);
-          setQuery('');
-          // console.log(result);
-        });
-    }
-  }
-
-  const dateDisplay = (d) => {
-    let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-    let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-
-    let day = days[d.getDay()];
-    let date = d.getDate();
-    let month = months[d.getMonth()];
-    let year = d.getFullYear();
-
-    return `${day} ${date} ${month} ${year}`
-  }
-
   return (
-    <div className="App">
-    <main>
-      <div className="search-box">
-        <input type="text"
-          className='search-bar'
-          placeholder='Search...'
-          onChange={e => setQuery(e.target.value)}
-          value={query}
-          onKeyPress={search}
-        /> 
-
-      </div>
-      {(typeof weather.main != 'undefined') ? (
-      <div>
-        <div className="location-box">
-          <div className='location'>{weather.name}, {weather.sys.country} </div>
-          <div className='date'>{dateDisplay(new Date())}</div>
-        </div>
-        <div className='weather-box'>
-          <div className='temp'>{Math.round(weather.main.temp)}°F</div>
-          <div className='weather'>{weather.weather[0].main}</div>
-          <div className='icon'><img
-              src={`http://openweathermap.org/img/w/${weather.weather[0].icon}.png`}
-              alt="weather status icon"
-              className="weather-icon"
-            /></div>
-        </div>
-      </div>
-      ) : ('')}
-    </main>
+    <div className='App'>
+      <WeatherCard />
     </div>
-  );
+  )
 }
 
 export default App;
